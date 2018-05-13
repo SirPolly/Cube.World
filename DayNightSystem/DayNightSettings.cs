d6e851f156d4853321e129771713e0e6aa61fb30
SirPolly <39922492+SirPolly@users.noreply.github.com>
2018-05-13 12:29:04 +0200

    Refactoring

--- DayNightSystem/DayNightSettings.cs
+++ DayNightSystem/DayNightSettings.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 
 namespace Cube.World {
+    [CreateAssetMenu(menuName = "Cube.World/DayNightSettings")]
     public class DayNightSettings : ScriptableObject {
         [Range(1f, 1000f)]
         public float dayLengthInSeconds;
